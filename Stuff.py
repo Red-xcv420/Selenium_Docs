17e049c9e09244ed39523b9fcadb6b5074d762df
Red-xcv420 <88535395+Red-xcv420@users.noreply.github.com>
2022-04-16 12:44:47 -0400

    Update Stuff.py

--- Stuff.py
+++ Stuff.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By```
+from selenium.webdriver.common.by import By
 #-------------------------
 #BETTER CODE
 #-------------------------
