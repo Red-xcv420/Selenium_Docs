f6f8302767c9cd941c770e540b4e81d041263570
Red-xcv420 <88535395+Red-xcv420@users.noreply.github.com>
2022-04-16 13:09:59 -0400

    Update Common_Errors.py

--- Common_Errors.py
+++ Common_Errors.py
@@ -1,1 +1,44 @@
-"""Coming soon."""
+#selenium.common.exceptions.JavascriptException: Message: javascript error: Invalid or unexpected token
+"""Are you trying to execute Javascript to the browser using selenium?, Make sure your Javascript is written correctly."""
+
+#AttributeError: 'NoneType' object has no attribute 'send_keys'
+"""
+Try these imports
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+"""
+
+#[<selenium.webdriver.remote.webelement.WebElement (session="1c402fdd805a809441060e8c5af66f93", element="ab74f36f-87d7-49c9-9669-63bb2a4f8790")>]
+"""
+Did you put .text, after you defined the element? Or did you just try to print it?
+
+Try this...
+
+Text_From_Element = WebDriverWait(driver, 3).until(
+        EC.presence_of_element_located((By.XPATH, 'xpath goes here'))).text
+print(Text_From_Element)
+"""
+
+#from session not created: This version of ChromeDriver only supports Chrome version 99
+"""
+The Chrome driver exe you downloaded does not work for your browser, The one you downloaded only supports 99, You have something else. 
+Check your browser version and get the one you need from https://sites.google.com/chromium.org/driver/
+"""
+
+#Do you have a squiggle line under the imports for selenium?
+"""
+Open CMD and run this...
+---
+
+pip install selenium
+
+---
+
+If This Doesnt Work https://www.youtube.com/watch?v=Ua743l1J3cg Follow This
+"""
