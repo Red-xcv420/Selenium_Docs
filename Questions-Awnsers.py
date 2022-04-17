cdebacd77fdf0dca3ef8c2ec8069b4e3d4b29df5
Red-xcv420 <88535395+Red-xcv420@users.noreply.github.com>
2022-04-16 20:21:20 -0400

    Create Questions-Awnsers.py

---
+++ Questions-Awnsers.py
@@ -0,0 +1,7 @@
+#Q When do I use xpath
+
+#A
+"""
+You shouldnt really use XPATH because it is very specific and will break your code if it is changed on the site, However, Other elements most of the time wont work
+So try the others first, if they dont work then try XPATH
+"""
