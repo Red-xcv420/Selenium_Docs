7215d59052089e58946f9535f2c349a212656032
Landen Fisher <88535395+Red-xcv420@users.noreply.github.com>
2022-05-24 00:31:09 -0400

    Create Timings_Example.py

---
+++ Examples/Anti_Detection/Components/Timings_Example.py
@@ -0,0 +1,10 @@
+#Import Random Abiltys
+import random 
+
+#User_Interaction_Times Var Set To A Random Number Between .20 And 2
+UIT = float(random.randrange(20, 200))/100
+
+#Print User_Interaction_Times
+print(UIT)
+
+#Example Outputs (0.20, 0.21, 0.95, 1.50, 1.05, 2)
