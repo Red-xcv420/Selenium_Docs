e486bf194355e4df73fc8dc091f29a82f8ace387
Landen Fisher <88535395+Red-xcv420@users.noreply.github.com>
2022-06-11 14:36:24 -0400

    Update Common_Warnings.py

--- Common_Warnings.py
+++ Common_Warnings.py
@@ -1,2 +1,5 @@
 #Passthrough is not supported, GL is disabled, ANGLE is
 """This can be ignored or disabled without harm."""
+
+#Bluetooth: bluetooth_adapter_winrt.cc:1074 Getting Default Adapter failed.
+"""This can be ignored or disabled without harm.""
